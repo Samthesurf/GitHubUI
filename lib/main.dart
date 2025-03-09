3f78aaa645f1b060acebbf9828d8312eea441c73
Samthesurf <ukpsamuel67@gmail.com>
2025-03-09 05:58:50 +0100

    Adding the shortcuts icons

--- lib/main.dart
+++ lib/main.dart
@@ -417,12 +417,13 @@ class GithubUI extends StatelessWidget {
                     ),
                   ),
                 ),
+                SizedBox(height: 10),
                 Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     Container(
-                      height: 30,
-                      width: 30,
+                      height: 35,
+                      width: 35,
                       decoration: BoxDecoration(
                         color: Colors.grey[800],
                         borderRadius: BorderRadius.all(
@@ -431,10 +432,115 @@ class GithubUI extends StatelessWidget {
                       ),
                       child: Icon(
                         OctIcons.zap,
-                        color: Colors.grey[350],
+                        color: Colors.grey[200],
                         size: 20,
                       ),
-                    )
+                    ),
+                    Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(220, 12, 101, 39),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                      child: Icon(
+                        OctIcons.issue_opened,
+                        color: Colors.green[200],
+                        size: 20,
+                      ),
+                    ),
+                    Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                        color:Colors.blue[900],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                      child: Icon(
+                        OctIcons.git_pull_request,
+                        color: Colors.blue[200],
+                        size: 20,
+                      ),
+                    ),
+                     Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                        color:Colors.purple[900],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                      child: Icon(
+                        OctIcons.comment_discussion,
+                        color: Colors.purple[200],
+                        size: 20,
+                      ),
+                    ),
+                    Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                        color:Colors.orange[900],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                      child: Icon(
+                        OctIcons.organization,
+                        color: Colors.orange[200],
+                        size: 20,
+                      ),
+                    ),
+                    Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                        color:Colors.pink[900],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                      child: Icon(
+                        OctIcons.people,
+                        color: Colors.pink[200],
+                        size: 20,
+                      ),
+                    ),
+                    Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                        color:Colors.purple[900],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                      child: Icon(
+                        OctIcons.briefcase,
+                        color: Colors.purple[200],
+                        size: 20,
+                      ),
+                    ),
+                    Container(
+                      height: 35,
+                      width: 35,
+                      decoration: BoxDecoration(
+                        color:Colors.grey[800],
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15.0),
+                        ),
+                      ),
+                      child: Icon(
+                        OctIcons.file_diff,
+                        color: Colors.grey[200],
+                        size: 17,
+                      ),
+                    ),
                   ],
                 )
               ],
