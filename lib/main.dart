72b721db0084cdbf7fc971a217f19514e17aa34b
Samthesurf <ukpsamuel67@gmail.com>
2025-03-09 08:29:35 +0100

    Refactor icon containers into a list and overlap the icons for a better look

--- lib/main.dart
+++ lib/main.dart
@@ -417,132 +417,21 @@ class GithubUI extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 30),
                 Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[800],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15.0),
+                    for (int i = 0; i < containers.length; i++)
+                      Align(
+                        widthFactor: 0.9,
+                        child: CircleAvatar(
+                          radius: 19.5,
+                          backgroundColor: Color.fromARGB(255, 23, 24, 27),
+                          child: containers[i],
                         ),
                       ),
-                      child: Icon(
-                        OctIcons.zap,
-                        color: Colors.grey[200],
-                        size: 20,
-                      ),
-                    ),
-                    Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(220, 12, 101, 39),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15.0),
-                        ),
-                      ),
-                      child: Icon(
-                        OctIcons.issue_opened,
-                        color: Colors.green[200],
-                        size: 20,
-                      ),
-                    ),
-                    Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                        color:Colors.blue[900],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15.0),
-                        ),
-                      ),
-                      child: Icon(
-                        OctIcons.git_pull_request,
-                        color: Colors.blue[200],
-                        size: 20,
-                      ),
-                    ),
-                     Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                        color:Colors.purple[900],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15.0),
-                        ),
-                      ),
-                      child: Icon(
-                        OctIcons.comment_discussion,
-                        color: Colors.purple[200],
-                        size: 20,
-                      ),
-                    ),
-                    Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                        color:Colors.orange[900],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15.0),
-                        ),
-                      ),
-                      child: Icon(
-                        OctIcons.organization,
-                        color: Colors.orange[200],
-                        size: 20,
-                      ),
-                    ),
-                    Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                        color:Colors.pink[900],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15.0),
-                        ),
-                      ),
-                      child: Icon(
-                        OctIcons.people,
-                        color: Colors.pink[200],
-                        size: 20,
-                      ),
-                    ),
-                    Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                        color:Colors.purple[900],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15.0),
-                        ),
-                      ),
-                      child: Icon(
-                        OctIcons.briefcase,
-                        color: Colors.purple[200],
-                        size: 20,
-                      ),
-                    ),
-                    Container(
-                      height: 35,
-                      width: 35,
-                      decoration: BoxDecoration(
-                        color:Colors.grey[800],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15.0),
-                        ),
-                      ),
-                      child: Icon(
-                        OctIcons.file_diff,
-                        color: Colors.grey[200],
-                        size: 17,
-                      ),
-                    ),
                   ],
-                )
+                ),
               ],
             ),
           ),
@@ -556,3 +445,82 @@ class GithubUI extends StatelessWidget {
     );
   }
 }
+
+List<Widget> containers = [
+  Container(
+    height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+      color: Colors.grey[800],
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Icon(OctIcons.zap, color: Colors.grey[200], size: 20),
+  ),
+  Container(
+    height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+      color: const Color.fromARGB(220, 12, 101, 39),
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Icon(OctIcons.issue_opened, color: Colors.green[200], size: 20),
+  ),
+  Container(
+    height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+      color: Colors.blue[900],
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Icon(OctIcons.git_pull_request, color: Colors.blue[200], size: 20),
+  ),
+  Container(
+    height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+      color: Colors.purple[900],
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Icon(
+      OctIcons.comment_discussion,
+      color: Colors.purple[200],
+      size: 20,
+    ),
+  ),
+  Container(
+    height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+      color: Colors.orange[900],
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Icon(OctIcons.organization, color: Colors.orange[200], size: 20),
+  ),
+  Container(
+    height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+      color: Colors.pink[900],
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Icon(OctIcons.people, color: Colors.pink[200], size: 20),
+  ),
+  Container(
+    height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+      color: Colors.purple[900],
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Icon(OctIcons.briefcase, color: Colors.purple[200], size: 20),
+  ),
+  Container(
+    height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+      color: Colors.grey[800],
+      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    ),
+    child: Icon(OctIcons.file_diff, color: Colors.grey[200], size: 17),
+  ),
+];
